6632b1114d66e50db499c1c42ff78da67b8d900d
Richard Smith <rsmith37@ggc.edu>
2021-01-02 11:56:36 -0500

    Created About Me link, home page link and changed from router.push to Link

--- static/components/Navbar.js
+++ static/components/Navbar.js
@@ -1,19 +1,26 @@
 import React from "react"
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 import NavbarLink from './NavbarLink'
 
 export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
+  const router = useRouter();
 
   return (
     <>
       <nav className="flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-gray-900 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <p
-              className="text-2xl lg:text-4xl md:text-3xl  font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
-            >
-              Richard Smith
-            </p>
+            <Link href="/">
+              <a
+                // href="/"
+                // onClick={() => router.push('/')}
+                className="text-2xl lg:text-4xl md:text-3xl  font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
+              >
+                Richard Smith
+              </a>
+            </Link>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -27,6 +34,9 @@ export default function Navbar({ fixed }) {
             id="example-navbar-danger"
           >
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <NavbarLink href="/about">
+                About Me
+              </NavbarLink>
               <NavbarLink href="/projects">
                 Projects
               </NavbarLink>
--- static/pages/about.js
+++ static/pages/about.js
@@ -3,8 +3,8 @@ import Layout from '../components/Layout'
 export default function About() {
   return (
     <Layout title="Richard Smith | About Me">
-      <div className="h-screen">
-        <h1>About Me</h1>
+      <div>
+        <h1 className="text-center text-5xl font-bold">About Me</h1>
       </div>
     </Layout>
   )
