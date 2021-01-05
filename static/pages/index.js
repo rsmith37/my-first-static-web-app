be45a18c4bcdc11453f7f47be1ce6b6f2b0478de
Richard Smith <rsmith37@ggc.edu>
2021-01-05 15:07:05 -0500

    removed red bg in navbar and add italics to home page

--- static/pages/index.js
+++ static/pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ test }) {
             <p className="py-2 text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-left animate-fadeIn2">Hello, I'm <span className="text-green-700 font-bold">Richard</span></p>
             <p className="py-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-left animate-fadeIn4">and welcome to my site!</p>
         </div>
-        <div className="pt-4 text-right text-xs sm:text-sm md:text-base lg:text-lg animate-fadeIn5">
+        <div className="pt-4 text-right text-xs sm:text-sm md:text-base lg:text-lg animate-fadeIn5 italic">
           Aspiring software developer. Avid learner. Tech enthusiast.
         </div>
       </div>
